8ff3b6ab6701364b2adb3d79e29d7eac39f62c65
Yasushi SHINOHARA <yasushi.shinohara@gmail.com>
2023-06-12 22:48:19 +0900

    First commit for putting it on stage.

---
+++ src/modules/constants.py
@@ -0,0 +1,9 @@
+# coding: UTF-8
+# This is created 2023/06/12 by Y. Shinohara
+# This is lastly modified YYYY/MM/DD by Y. Shinohara
+import numpy as np
+#Mathematicl constants
+pi  = np.pi
+tpi = 2.0*np.pi
+fpi = 4.0*np.pi
+zI  = 1.0j
